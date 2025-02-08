d031382ee4fbbfde052cdc3163882d5d1ae140ba
DavidJankovic00LIN <153224453+DavidJankovic00LIN@users.noreply.github.com>
2025-02-08 15:17:43 +0100

    -Dokumentacija V1
    -Dokumentacija V1

---
+++ Dokumentacija/PRikod/dialog.h
@@ -0,0 +1,39 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QObject> 
+#include <wiringPi.h>
+#include <stdio.h>
+#include <QTimer>  
+#include <QPushButton> 
+
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Dialog; }
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_radioButton_2_clicked();
+
+private:
+    Ui::Dialog *ui;
+
+    float merenje_brzine();  // Funkcija za merenje brzine
+};
+#endif // DIALOG_H
